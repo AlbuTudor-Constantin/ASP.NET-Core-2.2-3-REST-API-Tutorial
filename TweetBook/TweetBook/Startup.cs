ce1f1c4b0a6b0610904ae8813015a4312cc5b1d7
WhiteCode12 <tudoralbu21@gmail.com>
2023-06-23 14:23:05 +0300

    Dependency injection and clean service registration | ASP.NET 5 REST API Tutorial 3

--- TweetBook/TweetBook/Startup.cs
+++ TweetBook/TweetBook/Startup.cs
@@ -14,6 +14,7 @@ using TweetBook.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Swashbuckle.AspNetCore.Swagger;
+using TweetBook.Installers;
 using SwaggerOptions = TweetBook.Options.SwaggerOptions;
 
 namespace TweetBook
@@ -30,18 +31,7 @@ namespace TweetBook
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<DataContext>(options =>
-                options.UseSqlite(
-                    Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>()
-                .AddEntityFrameworkStores<DataContext>();
-
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-
-            services.AddSwaggerGen(x =>
-            {
-                x.SwaggerDoc("v1", new Info { Title = "TweetBoop API", Version = "v1" });
-            });
+            services.InstallServicesInAssembly(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

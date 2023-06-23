f9208cbf2555921461761ddb9cf95d594d7d790a
WhiteCode12 <tudoralbu21@gmail.com>
2023-06-23 14:12:19 +0300

    Implementing versioning | ASP.NET Core 5 REST API Tutorial 2

---
+++ TweetBook/TweetBook/Contracts/ApiRoutes.cs
@@ -0,0 +1,16 @@
+﻿namespace TweetBook.Contracts
+{
+    public static class ApiRoutes
+    {
+        private const string Route = "api";
+        
+        private const string Version = "v1";
+        
+        private const string Base = Route + "/" + Version;
+
+        public static class Post
+        {
+            public const string GetAll = Base + "/posts";
+        }
+    }
+}
---
+++ TweetBook/TweetBook/Controllers/V1/PostsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using TweetBook.Contracts;
+using TweetBook.Domain;
+
+namespace TweetBook.Controllers.V1
+{
+    public class PostsController : Controller
+    {
+        private readonly List<Post> _posts;
+        
+        public PostsController()
+        {
+            _posts = new List<Post>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                _posts.Add(new Post { Id = Guid.NewGuid().ToString() });
+            }
+        }
+        
+        [HttpGet(ApiRoutes.Post.GetAll)]
+        public IActionResult GetAll()
+        {
+            return Ok(_posts);
+        }
+    }
+}

2f4c1a7b1192e66470cd3218c076216dd9de6b6a
WhiteCode12 <tudoralbu21@gmail.com>
2023-06-25 16:32:33 +0300

    Integration testing | ASP.NET Core 5 REST API Tutorial 15

---
+++ TweetBook/TweetBook.IntegrationTest/PostsControllerTests.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using FluentAssertions;
+using TweetBook.Contracts.V1;
+using TweetBook.Contracts.V1.Requests;
+using TweetBook.Contracts.V1.Responses;
+using TweetBook.Domain;
+using Xunit;
+
+namespace TweetBook.IntegrationTest
+{
+    public class PostsControllerTests : IntegrationTest
+    {
+        [Fact]
+        public async Task GetAll_WithoutAnyPosts_ReturnsEmptyResponse()
+        {
+            // Arrange
+            await AuthenticateAsync();
+
+            // Act
+            var response = await TestClient.GetAsync(ApiRoutes.Post.GetAll);
+
+            //Assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            (await response.Content.ReadAsAsync<List<Post>>()).Should().BeEmpty();
+        }
+
+        [Fact]
+        public async Task Get_ReturnsPost_WhenPostsExistsInTheDatabase()
+        {
+            await AuthenticateAsync();
+            var createdPost = await CreatePostAsync(new CreatePostRequest
+            {
+                Name = "Test post"
+            });
+
+            var response = await TestClient.GetAsync(ApiRoutes.Post.Get.Replace("{postId}", createdPost.Id.ToString()));
+
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            var returnedPost = await response.Content.ReadAsAsync<Post>();
+            returnedPost.Id.Should().Be(createdPost.Id);
+            returnedPost.Name.Should().Be("Test post");
+        }
+    }
+}
